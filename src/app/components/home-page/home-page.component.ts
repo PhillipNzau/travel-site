90d9bcb8832cbb821243184f2259e7762bfd43cf
phillip nzau junior <nzau78@gmail.com>
2021-08-14 18:15:24 +0300

    gallery viewer diff width

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -7,24 +7,32 @@ import {logger} from 'codelyzer/util/logger';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  width: any;
-  height: any;
+  // width: any;
+  // height: any;
+  myObjArray = [{
+    width: '800',
+    height: '200',
+  }];
+  myObjSliced =  [{
+    width: '800',
+    height: '200',
+  }];
 
   constructor() { }
 
   ngOnInit(): void {
-
-
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < 9; i++){
+      this.myObjArray.push({
+        width: (Math.random() * (1000 - 500) + 500).toFixed(),
+        height: (Math.random() * (1000 - 200) + 200).toFixed(),
+      });
+    }
+    this.myObjSliced = this.myObjArray.slice(0, 8);
   }
-
   numSeq(n: number): Array<number> {
-
     return Array(n);
   }
-  dimSeq(n: number): Array<number> {
 
-    console.log('ran', this.width);
-    return Array(n);
-  }
 
 }
