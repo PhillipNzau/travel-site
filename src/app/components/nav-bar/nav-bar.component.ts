06aa5b9ce32b0dfbdde229c55203aded9777d1bf
phillip nzau junior <nzau78@gmail.com>
2021-09-07 15:33:39 +0300

    nav fix

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,20 +7,20 @@ import {Component, HostListener, OnInit} from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   isOpen = false;
-  @HostListener('window:scroll', ['$event'])
+  // @HostListener('window:scroll', ['$event'])
 
   // tslint:disable-next-line:typedef
-  onWindowScroll() {
-    const element =  document.querySelector('.navbar') as HTMLElement;
-    if (window.pageYOffset > 2) {
-      element.classList.add('bg-dark');
-      // element.classList.remove('bg-darks');
-      this.isOpen = false;
-      console.log('scolled', this.isOpen);
-    } else {
-      element.classList.remove('bg-dark');
-    }
-  }
+  // onWindowScroll() {
+  //   const element =  document.querySelector('.navbar') as HTMLElement;
+  //   if (window.pageYOffset > 2) {
+  //     element.classList.add('bg-dark');
+  //     // element.classList.remove('bg-darks');
+  //     this.isOpen = false;
+  //     console.log('scolled', this.isOpen);
+  //   } else {
+  //     element.classList.remove('bg-dark');
+  //   }
+  // }
 
   constructor() { }
 
@@ -28,13 +28,13 @@ export class NavBarComponent implements OnInit {
   }
 
   // tslint:disable-next-line:typedef
-  clicked() {
-    const element =  document.querySelector('.navbar') as HTMLElement;
-    if (!this.isOpen) {
-      element.classList.add('bg-dark');
-      console.log('is open', !this.isOpen);
-    }
-    else {
-    }
-  }
+  // clicked() {
+  //   const element =  document.querySelector('.navbar') as HTMLElement;
+  //   if (!this.isOpen) {
+  //     element.classList.add('bg-dark');
+  //     console.log('is open', !this.isOpen);
+  //   }
+  //   else {
+  //   }
+  // }
 }
